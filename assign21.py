99a5cab72303bcf0d37b8226dd809a783caf89df
Likhita-P <66911677+Likhita-P@users.noreply.github.com>
2020-09-04 16:14:14 +0530

    Add files via upload

---
+++ assign21.py
@@ -0,0 +1,11 @@
+#question 1 print pattern
+n=5
+for i in range(0,n):
+    for j in range(0,i+1):
+        print("*",end=" ")
+    print()
+for i in range(n-1,-1,-1):
+    for j in range(0,i):
+        print("*",end=" ")
+    print()
+
---
+++ assign22.py
@@ -0,0 +1,3 @@
+#question 2
+var=input("Enter word")
+print(var[::-1])
